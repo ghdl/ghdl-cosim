95ab57e5a4cb52c7d0aaf6a3972c46ccfbf04601
umarcor <unai.martinezcorral@ehu.eus>
2020-04-08 16:13:22 +0200

    vhpidirect: add 'Dynamic/Generating shared libraries', update shared/shghdl

--- vhpidirect/shared/shghdl/main.c
+++ vhpidirect/shared/shghdl/main.c
@@ -4,15 +4,27 @@
 
 int main(int argc, void** argv) {
 
+  void* h = dlopen("./tb.so", RTLD_LAZY);
+  if (!h){
+    fprintf(stderr, "%s\n", dlerror());
+    exit(1);
+  }
+
+
+  typedef int print_t(char* str);
+
+  print_t* print_something = (print_t*)dlsym(h, "print_something");
+  if (!print_something){
+    fprintf(stderr, "%s\n", dlerror());
+    exit(2);
+  }
+
+  print_something("Hello Something!");
+
+
   typedef int main_t(int, void**);
 
   for (size_t i = 0; i < 3; i++) {
-
-    void* h = dlopen("./tb.so", RTLD_LAZY);
-    if (!h){
-      fprintf(stderr, "%s\n", dlerror());
-      exit(1);
-    }
 
     main_t* ghdl_main = (main_t*)dlsym(h, "ghdl_main");
     if (!ghdl_main){
@@ -23,6 +35,14 @@ int main(int argc, void** argv) {
     printf("ghdl_main return: %d\n", ghdl_main(argc, argv));
 
     dlclose(h);
+
+    if (i<2) {
+      h = dlopen("./tb.so", RTLD_LAZY);
+      if (!h){
+        fprintf(stderr, "%s\n", dlerror());
+        exit(1);
+      }
+    }
 
   }
 
