976dcf5bb6c7ba3f13d21cde84a41bef60facb5a
Andrea Bellandi <andrea.bellandi@desy.de>
2020-05-01 11:39:58 +0200

    corrected multiple drive issue in vpi/list/tb.vhd

--- vpi/list/tb.vhd
+++ vpi/list/tb.vhd
@@ -7,34 +7,35 @@ end tb;
 
 architecture behavioral of tb is
     
-        signal nibble1, nibble2, sum : unsigned(3 downto 0);
-        signal carry_out : std_logic;
+        signal nibble1, nibble2 : unsigned(3 downto 0);
+        signal sum_0, sum_1, sum_2, sum_3 : unsigned(3 downto 0);
+        signal carry_out_0, carry_out_1, carry_out_2, carry_out_3: std_logic;
 
     begin
 
         ent_0: entity work.ent 
                     port map (nibble1 => nibble1,
                              nibble2 => nibble2,
-                             sum => sum,
-                             carry_out => carry_out);
+                             sum => sum_0,
+                             carry_out => carry_out_0);
 
         ent_1: entity work.ent 
                     port map (nibble1 => nibble1,
                              nibble2 => nibble2,
-                             sum => sum,
-                             carry_out => carry_out);
+                             sum => sum_1,
+                             carry_out => carry_out_1);
 
         ent_2: entity work.ent 
                     port map (nibble1 => nibble1,
                              nibble2 => nibble2,
-                             sum => sum,
-                             carry_out => carry_out);
+                             sum => sum_2,
+                             carry_out => carry_out_2);
 
         ent_3: entity work.ent 
                     port map (nibble1 => nibble1,
                              nibble2 => nibble2,
-                             sum => sum,
-                             carry_out => carry_out);
+                             sum => sum_3,
+                             carry_out => carry_out_3);
         process
 
         begin
