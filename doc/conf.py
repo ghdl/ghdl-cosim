25f9f049c537e04f2b5e94b573779d555ef89112
umarcor <unai.martinezcorral@ehu.eus>
2020-07-09 16:53:21 +0200

    ci: use buildthedocs/btd to build and publish the docs to GHP

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import sys, re
-from os.path import abspath, join
+from os.path import abspath
+from pathlib import Path
+from json import dump, loads
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -25,7 +27,7 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
-templates_path = ['_templates', '_themes']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 source_suffix = {
@@ -78,16 +80,21 @@ except Exception as ex:
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
+html_logo = "_static/logow.svg"
 
-# Override default css to get a larger width for ReadTheDoc build
-html_context = {
-    'css_files': [
-        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
-        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-        '_static/theme_overrides.css',
-    ],
+html_theme_options = {
+    'logo_only': True,
+    'home_breadcrumbs': False,
+    'vcs_pageview_mode': 'blob',
 }
+
+html_context = {}
+ctx = Path(__file__).resolve().parent / 'context.json'
+if ctx.is_file():
+    html_context.update(loads(ctx.open('r').read()))
+
+html_theme_path = ["."]
+html_theme = "_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
