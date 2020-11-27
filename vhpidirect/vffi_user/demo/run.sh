6cebe9a4f2e3e82a6767cbd373026aed93e8ab38
umarcor <unai.martinezcorral@ehu.eus>
2020-11-27 06:37:55 +0100

    vhpidirect/vffi_user/demo: fix log groups

--- vhpidirect/vffi_user/demo/run.sh
+++ vhpidirect/vffi_user/demo/run.sh
@@ -10,9 +10,13 @@ ghdl -a --std=08 -O0 -g valuesFromVhdlToC.vhd
 echo "Build tb (with valuesFromVhdlToC.c and headers)"
 ghdl -e --std=08 -O0 -g -Wl,-I../../ -Wl,valuesFromVhdlToC.c -o tb_valuesFromVhdlToC tb_valuesFromVhdlToC
 
+[ -n "$CI" ] && echo '::endgroup::' || true
+
 echo "Execute tb"
 ./tb_valuesFromVhdlToC
 
+
+[ -n "$CI" ] && echo '::group::Log' || true
 
 echo "Analyze valuesFromVhdlToC.vhd"
 ghdl -a --std=08 -O0 -g accessesFromVhdlToC.vhd
@@ -20,5 +24,9 @@ ghdl -a --std=08 -O0 -g accessesFromVhdlToC.vhd
 echo "Build tb (with accessesFromVhdlToC.c and headers)"
 ghdl -e --std=08 -O0 -g -Wl,-I../../ -Wl,accessesFromVhdlToC.c -o tb_accessesFromVhdlToC tb_accessesFromVhdlToC
 
+[ -n "$CI" ] && echo '::endgroup::' || true
+
 echo "Execute tb"
 ./tb_accessesFromVhdlToC
+
+[ -n "$CI" ] && echo '::group::End' || true
