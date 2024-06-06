74c2e51d5af252ec38a96f465e11c9eda5cb5470
umarcor <unai.martinezcorral@ehu.eus>
2024-06-06 23:14:53 +0200

    vhpidirect/arrays/matrices/vunit_axis_vcs: 'ghdl.flags' was removed in VUnit v5.0.0; use 'ghdl.a_flags'

--- vhpidirect/arrays/matrices/vunit_axis_vcs/run.py
+++ vhpidirect/arrays/matrices/vunit_axis_vcs/run.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parent
 
 VU.add_library("lib").add_source_files([ROOT.parent / "pkg.vhd", ROOT / "src" / "*.vhd", ROOT / "*.vhd"])
 
-VU.set_compile_option("ghdl.flags", ["-frelaxed"])
+VU.set_compile_option("ghdl.a_flags", ["-frelaxed"])
 # Add the C wrapper to the elaboration of GHDL
 VU.set_sim_option("ghdl.elab_flags", ["-frelaxed", "-Wl," + str(ROOT.parent / "main.c")])
 
